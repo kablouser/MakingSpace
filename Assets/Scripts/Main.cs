717e494e4383e3bdd2b23faf4258facd00d4f4e9
kablouser <kablouser231@gmail.com>
2023-09-30 23:51:53 +0100

    Engine Speed And Victory Screen

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -1,18 +1,58 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Main : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Victory Condition")]
+    public float distanceTravelled;
+    public float destinationDistance;
+    public float speedPerEngine;
+
+    [System.Serializable]
+    public struct Engine
+    {
+        public uint health;
+    }
+    public Engine[] engines = new Engine[2];
+    public uint GetActiveEngines()
+    {
+        uint count = 0;
+        foreach (Engine engine in engines)
+        {
+            if (0 < engine.health)
+                count++;            
+        }
+        return count;
+    }
+
+    public Slider destinationDistanceSlider;
+    public GameObject victoryScreen;
+
+
+    public void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Update()
     {
-        
+        if (distanceTravelled < destinationDistance)
+        {
+            uint activeEngines = GetActiveEngines();
+            if (0 < activeEngines)
+            {
+                distanceTravelled += Time.deltaTime * activeEngines * speedPerEngine;
+                destinationDistanceSlider.value = distanceTravelled / destinationDistance;
+            }
+            victoryScreen.SetActive(false);
+        }
+        else
+        {
+            // win
+            victoryScreen.SetActive(true);
+            destinationDistanceSlider.value = 1f;
+        }
     }
 }
